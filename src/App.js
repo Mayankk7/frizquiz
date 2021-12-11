9ee0270303d6273268f37b67bf84ad851de38a99
Mayankk7 <mayank26032@gmail.com>
2021-12-12 01:48:49 +0530

    trial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Home from './components/Home';
+import Start from './components/Start';
 import Quiz from './components/Quiz';
 import Result from './components/Result';
 
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route exact path="/home" element={<Home/>}/>
+          <Route exact path="/start" element={<Start/>}/>
         </Routes>
         <Routes>
           <Route exact path="/quiz" element={<Quiz score={score} setscore={setscore}/>}/>
