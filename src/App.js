49d55e5b7b386ad80afd7af7c0151bc7bd308160
Mayankk7 <mayank26032@gmail.com>
2021-12-12 01:50:16 +0530

    Revert "trial commit"
    This reverts commit 9ee0270303d6273268f37b67bf84ad851de38a99.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Start from './components/Start';
+import Home from './components/Home';
 import Quiz from './components/Quiz';
 import Result from './components/Result';
 
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route exact path="/start" element={<Start/>}/>
+          <Route exact path="/home" element={<Home/>}/>
         </Routes>
         <Routes>
           <Route exact path="/quiz" element={<Quiz score={score} setscore={setscore}/>}/>
