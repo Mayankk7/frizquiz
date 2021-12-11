cf7a6215b42faab2b42ad7b96b9fe6b230163190
Mayankk7 <mayank26032@gmail.com>
2021-12-11 18:41:10 +0530

    adding navigation and option selection

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,10 +1,15 @@
 import React,{useState} from 'react'
+import { useEffect } from 'react/cjs/react.development';
 import data from "../data.json";
 const Quiz = () => {
     
     const [currentques, setcurrentques] = useState(0);
+    const [correct,setcorrect] = useState(false);
+    const [select, setselect] = useState(false)
     var selectedoption = "";
+    var index=1000;
     const nextques = () => {
+    setselect(false);
       if (currentques == 9) {
         console.log("Quiz Ended");
         alert("Quiz Ended");
@@ -16,6 +21,7 @@ const Quiz = () => {
     }
   
     const prevques = () => {
+        setselect(false);
       if (currentques == 0) {
         alert("Cant do that!");
         setcurrentques(0);
@@ -28,13 +34,24 @@ const Quiz = () => {
   
   
     }
-  
+    
+
     const showans = () => {
       console.log(selectedoption);
+      if(index==1000){
+          alert("Please Select an option");
+          return;
+      }
+      setselect(true);
+      const id =document.getElementById("btn"+`${index}`)
       if(selectedoption==data[currentques].correct_answer){
+        setcorrect(true);
         console.log("Correct");
+        id.classList.toggle('alert-success');
       }else{
+          setcorrect(false)
           console.log("Incorrect Selection");
+          id.classList.add('alert-danger');
       }
     }
   
@@ -49,17 +66,20 @@ const Quiz = () => {
         <div class="card-body">
           {data[currentques].options.map((option,idx) => {
             return (
-              <button className='btn' onClick={(e) => {
+              <button className="btn" id={"btn"+`${idx}`} onClick={(e) => {
                 selectedoption=option;
+                index=idx;
               }}>{option}</button>
             )
           })
           }
         </div>
         <div class="card-footer text-muted">
-          <button className='btn btn-dark' onClick={() => prevques()}>Prev</button>
-          <button className='btn btn-dark mr-5' onClick={() => showans()}>Ok</button>
-          <button className='btn btn-dark' onClick={() => nextques()}>Next</button>
+          <button className='btn btn-dark' onClick={() => {prevques(); setcorrect(false);}}>Prev</button>
+          <button className='btn btn-dark mr-5' onClick={() => {showans(); }}>Ok</button>
+          <button className='btn btn-dark' onClick={() => {
+              select ? (nextques()) : (alert("Please Select an option"))
+          }}>Next</button>
         </div>
       </div>
         </div>
