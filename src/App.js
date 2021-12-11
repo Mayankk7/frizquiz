e357d231b4a3014fc8eec001765c5f8a1f3d27e4
Mayankk7 <mayank26032@gmail.com>
2021-12-12 01:45:21 +0530

    changine routes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Home from './components/Home';
 import Quiz from './components/Quiz';
 import Result from './components/Result';
+
 function App() {
   const [score,setscore] = useState(0);
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route exact path="" element={<Home/>}/>
+          <Route exact path="/home" element={<Home/>}/>
         </Routes>
         <Routes>
           <Route exact path="/quiz" element={<Quiz score={score} setscore={setscore}/>}/>
