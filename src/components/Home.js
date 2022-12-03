386b231b3ff69a3f515ee3b3f47ec9e967197e5f
mayankk7 <mayank26032@gmail.com>
2022-12-04 01:48:24 +0530

    Error handling

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,10 @@ const Home = () => {
     const [name,setname] = useState('');
 
     const startquiz = () => {
+        if(name===""){
+            alert("Kindly Enter your name");
+            return;
+        }
         localStorage.setItem('name',name);
         window.location.href="/quiz";
     }
@@ -13,7 +17,7 @@ const Home = () => {
             <h1 className='head'>FRIZQUIZ</h1>
             <div className='mt-5'>
             <input type="text" className="mx-3" style={{borderRadius:"5px"}} placeholder='Name' onChange={(e)=> {setname(e.target.value);}}/>
-            <button className='btn mt-2' onClick={() => {startquiz()}}>Start!</button>
+            <button className='btn ' onClick={() => {startquiz()}}>Start!</button>
             </div> 
             </div>
 
